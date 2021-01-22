3ae3f7c0d28f934f3f0e7295e79dd8188ee66ffb
AdamDriver26 <adamjdriver26@gmail.com>
2021-01-22 17:20:55 +0000

    Finished the flood fill algortihm and implemented the first version of the interactive graphics

--- Path.py
+++ Path.py
@@ -1,7 +1,8 @@
 from MapLogic import *
+import random
 
-pathMap = Map([8,8],20)
-obstacleList = [4,5,7,10,13,18,19,23,26,29,36,37,39,42,45,47,50,51,55,61]
+pathMap = Map([16,16],20)
+obstacleList = [219, 218, 217, 215, 214, 213, 216, 182, 166, 200, 184, 239, 238, 240, 204, 205, 206, 236, 252, 220, 208, 192, 176, 160, 155, 171, 187, 203]
 for obs in obstacleList:
   pathMap.regionList[obs].traverse = False
 
@@ -12,14 +13,14 @@ def floodFill(grid, initRef, termRef):
   """
   queue = [ [initRef, 0] ]
   checked = [initRef]
-  step = 1
+  fStep = 1
   nCount = 0
   a = 0
   b = 1
-  maxIterations = 50
+  maxIterations = 1000
 
   # Cycles until either the terminal point (with regionList reference termRef) is found, or the maximum number of iterations is reached.
-  while ([termRef, step - 1] not in queue) and (step <= maxIterations):
+  while ([termRef, fStep - 1] not in queue) and (fStep <= maxIterations):
 
     # The values [a,b) reflect the elements of queue that have not had their neighbours considered.
     for i in range(a,b):
@@ -33,7 +34,7 @@ def floodFill(grid, initRef, termRef):
 
         if (grid.regionList[neighbour].traverse == True) and (neighbour not in checked):
           print(neighbour, " added")
-          queue.append([neighbour,step])
+          queue.append([neighbour,fStep])
           checked.append(neighbour)
           nCount += 1
 
@@ -42,13 +43,26 @@ def floodFill(grid, initRef, termRef):
     a = b
     b += nCount
     nCount -= nCount
-    step += 1
+    fStep += 1
 
   # The path is then initiated with the terminal point, from which the next steps will work backwards from.
   orderedPath = [termRef]
+  # The step at which the termRef was found.
+  maxStep = queue[-1][1]
 
+  # Iterates backwards in range (-1,maxStep] = [0,maxStep], as the path is found backwards from the terminal point, given by termRef.
+  for bStep in range(maxStep - 1,-1,-1):
+    neighbours = grid.neighbours(orderedPath[0])
+    options = []
+
+    for neighbour in neighbours:
+      if [neighbour, bStep] in queue:
+        options.append(neighbour)
+    
+    orderedPath.insert(0, random.choice(options))
+    print("path:",orderedPath,"at step:",bStep)
   
-  return queue
+  return [orderedPath, queue]
 
 
-print(floodFill(pathMap,17,59))
+print(floodFill(pathMap,247,256))
--- Render.py
+++ Render.py
@@ -2,17 +2,17 @@ from tkinter import *
 import time
 from MapLogic import *
 from Path import *
+import random
 
-renderMap = Map([8,8],80)
-
-obstacleList = [4,5,7,10,13,18,19,23,26,29,36,37,39,42,45,47,50,51,55,61]
-
-for obs in obstacleList:
-  renderMap.regionList[obs].traverse = False
+renderMap = Map([20,16],50)
 
 X = renderMap.dim[0]*renderMap.div
 Y = renderMap.dim[1]*renderMap.div
 h = renderMap.div # Grid division width
+
+initReg = random.randrange(0,renderMap.dim[0]*renderMap.dim[1]+1)
+termReg = random.choice([random.randrange(0,initReg), random.randrange(initReg + 1, renderMap.dim[0]*renderMap.dim[1]+1)])
+
 
 drawnObstacles = {}
 
@@ -21,13 +21,44 @@ def toggleObstacle(event):
   if reg.ref in drawnObstacles:
     canvas.delete("obstacle" + str(reg.ref))
     drawnObstacles.pop(reg.ref)
+    renderMap.regionList[reg.ref].traverse = True
 
   else:
-    reg.traverse = False
+    renderMap.regionList[reg.ref].traverse = False
     drawnObstacles[reg.ref] = canvas.create_rectangle([i*h for i in reg.corners[0] + reg.corners[2]], fill="grey20", outline="", tags="obstacle" + str(reg.ref))
 
 def printObstacles(event):
-  print(drawnObstacles)
+  print(drawnObstacles.keys())
+
+def genPath(event):
+  path, queue = floodFill(renderMap,initReg,termReg)
+  colours = colourGradient(len(path))
+  print(path)
+  print(queue)
+  for x in queue:
+    reference = x[0]
+    region = renderMap.regionList[reference]
+    canvas.create_rectangle([i*h for i in region.corners[0] + region.corners[2]], fill=colours[x[1]], outline="")
+  
+  for j in range(0,len(path)-1):
+    canvas.create_line([k*h for k in renderMap.regionList[path[j]].centre + renderMap.regionList[path[j+1]].centre], fill="#094481", width=4)
+    #canvas.create_oval([renderMap.regionList[path[j+1]].corners[0]*h-2, renderMap.regionList[path[j+1]].corners[1]*h-2, renderMap.regionList[path[j+1]].corners[0]*h+2, renderMap.regionList[path[j+1]].corners[1]*h+2], fill="#094481", width=4)
+
+
+
+
+
+def colourGradient(maxSteps):
+  startGreenRGB = [250,250,110]
+  endRedRGB = [159,21,21]
+  output = ['#%02x%02x%02x' % (startGreenRGB[0], startGreenRGB[1], startGreenRGB[2])]
+
+  for i in range(1,maxSteps):
+    output.append('#%02x%02x%02x' % (startGreenRGB[0]+i*(endRedRGB[0]-startGreenRGB[0])//maxSteps, startGreenRGB[1]+i*(endRedRGB[1]-startGreenRGB[1])//maxSteps, startGreenRGB[2]+i*(endRedRGB[2]-startGreenRGB[2])//maxSteps))
+    
+  output.append('#%02x%02x%02x' % (endRedRGB[0], endRedRGB[1], endRedRGB[2]))
+
+  return output
 
 gui = Tk()
 gui.geometry(str(X) + "x" + str(Y))
@@ -36,6 +67,7 @@ gui.title("Pathfinder")
 canvas = Canvas(gui, width=X, height=Y, bg='grey90')
 canvas.bind("<Button-1>", toggleObstacle)
 canvas.bind("<Button-3>", printObstacles)
+gui.bind("<Return>", genPath)
 canvas.pack()
 
 # Creates vertical grid lines
@@ -46,15 +78,9 @@ for i in range(0,X,h):
 for j in range(0,Y,h):
   canvas.create_line(0,j,X,j, fill="grey80", width=2)
 
-colors = {} #values 0-10 with gradually darker colours
-
-queue = floodFill(renderMap,17,59)
-
-# print(queue)
-
-for x in queue:
-  reference = x[0]
-  region = renderMap.regionList[reference]
-  canvas.create_oval([i*h for i in region.corners[0] + region.corners[2]], fill="black")
+# Create the start and end point rectangles.
+canvas.create_rectangle([i*h for i in renderMap.regionList[initReg].corners[0] + renderMap.regionList[initReg].corners[2]], fill="#af3448")
+canvas.create_rectangle([i*h for i in renderMap.regionList[termReg].corners[0] + renderMap.regionList[termReg].corners[2]], fill="#360981")
 
 gui.mainloop()
+
