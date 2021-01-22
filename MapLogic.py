7fd456738c3e0eaca463bafc0c30c4c20f872824
AdamDriver26 <adamjdriver26@gmail.com>
2021-01-22 14:32:04 +0000

    Some tidying and commenting has been completed

--- MapLogic.py
+++ MapLogic.py
@@ -1,26 +1,38 @@
 import math
 
 class Region:
-  def __init__(self, ref, centre, corners, traverse):
+  def __init__(self, ref, centre, corners):
     """
     Represents a division of the map which can traversable by the "creature" or an obstacle.
     Args:
       ref (int): a *unique* reference for a single region.
       centre (List[int]): the relative coordinates of the regions centre.
       corners (List[int]): the relative coordinates of the regions four vertices, counted clockwise starting from the top left.
-      traverse (Boolean): whether the region is an obstacle (False) or traversable (True).
+      traverse (Boolean): whether the region is an obstacle (False) or traversable (True). True by default.
     """
     self.ref = ref
     self.centre = centre
     self.corners = corners
-    self.traverse = traverse
+    self.traverse = True
 
   @staticmethod
   def disVector(a, b):
+    """
+    Returns a vector from the centre of region a to the centre of region b.
+    Args:
+      a (Region): The starting region.
+      b (Region): The terminal region.
+    """
     return [b.centre[0] - a.centre[0], b.centre[1] - a.centre[1]]
 
   @staticmethod
   def disMetric(a, b):
+    """
+    Returns the Euclidean distance between the centre of region a and the centre of region b.
+    Args:
+      a (Region): The starting region.
+      b (Region): The terminal region.
+    """
     return math.sqrt((b.centre[0] - a.centre[0])**2 + (b.centre[1] - a.centre[1])**2)
 
 
@@ -39,16 +51,30 @@ class Map:
     self.generateRegions()
 
   def generateRegions(self):
+    """
+    This method generates regions with unique references for all grid points as defined by dim. Method is
+    automatically called upon creation of a Map object instance.
+    """
     currentRef = 1
     for relY in range(self.dim[1]):
       for relX in range(self.dim[0]):
-        self.regionList[currentRef] = Region(currentRef,[relX+0.5,relY+0.5],[[relX,relY],[relX+1,relY],[relX+1,relY+1],[relX,relY+1]],True)
+        self.regionList[currentRef] = Region(currentRef,[relX+0.5,relY+0.5],[[relX,relY],[relX+1,relY],[relX+1,relY+1],[relX,relY+1]])
         currentRef += 1
 
   def objCoordinateToRef(self,pos):
+    """
+    Given a set of objective coordinates determines the reference of the region that contains those coordinates.
+    Args:
+      pos (List[int]): The objective coordinates, assumed to be within [0,dim[0]]x[0,dim[1]].
+    """
     return self.dim[0]*(pos[1]//self.div) + (pos[0]//self.div) + 1
 
   def neighbours(self, ref):
+    """
+    Given a region reference returns a list of that region's neighbouring regions on the Map object.
+    Args:
+      ref (int): The reference of the region to find the neighbours of. 
+    """
     neighboursList = []
     if ref % self.dim[0] == 0:                  # Checks that the region is in the Eastern column
 
--- Path.py
+++ Path.py
@@ -1,15 +1,15 @@
 from MapLogic import *
 
 pathMap = Map([8,8],20)
-
-# obstacleList = [56, 54, 30, 43, 42, 45, 44, 67, 55, 41, 53, 52, 40, 29, 51, 64, 66, 65, 77, 79, 78, 68] # for 12,10
 obstacleList = [4,5,7,10,13,18,19,23,26,29,36,37,39,42,45,47,50,51,55,61]
-
 for obs in obstacleList:
   pathMap.regionList[obs].traverse = False
 
 
 def floodFill(grid, initRef, termRef):
+  """
+
+  """
   queue = [ [initRef, 0] ]
   checked = [initRef]
   step = 1
@@ -18,17 +18,17 @@ def floodFill(grid, initRef, termRef):
   b = 1
   maxIterations = 50
 
-  # for reg in grid.regionList:
-  #   if grid.regionList[reg].traverse == True:
-  #     unchecked.append(grid.regionList[reg].ref)
-
+  # Cycles until either the terminal point (with regionList reference termRef) is found, or the maximum number of iterations is reached.
   while ([termRef, step - 1] not in queue) and (step <= maxIterations):
-    print("a:",a,"b:",b,"nCount:",nCount)
+
+    # The values [a,b) reflect the elements of queue that have not had their neighbours considered.
     for i in range(a,b):
 
+      # Uses the neighbours method of MapLogic to find the neighbouring regions of the current element in the queue. 
       neighbours = grid.neighbours(queue[i][0])
-      print("unchecked neighbours of ",queue[i][0]," at position ",i,": ",neighbours)
 
+      # Iterates through these neighbours and checks that they: a) are not an obstacle; and b) have not been checked previously.
+      # Adds the successful neighbours to the queue with a note of the step and iterates the nCount.
       for neighbour in neighbours:
 
         if (grid.regionList[neighbour].traverse == True) and (neighbour not in checked):
@@ -37,17 +37,18 @@ def floodFill(grid, initRef, termRef):
           checked.append(neighbour)
           nCount += 1
 
-        print(nCount)
-
-      print("step:",step,", queue:",queue)
+    # After all elements of queue added in the last cycle have had their neighbours considered, updates the new values of a & b;
+    # resets the nCount; and iterates the current step.
     a = b
     b += nCount
     nCount -= nCount
     step += 1
+
+  # The path is then initiated with the terminal point, from which the next steps will work backwards from.
+  orderedPath = [termRef]
+
   
   return queue
 
 
-floodFill(pathMap,17,59)
-
-#[[59, 0], [58, 1], [60, 1], [57, 2], [52, 2], [49, 2], [44, 3], [53, 3], [41, 3], [43, 3], [54, 4], [33, 4], [35, 4], [46, 4], [62, 4], [25, 5], [34, 5], [27, 5], [38, 5], [63, 5], [17, 5]]
+print(floodFill(pathMap,17,59))
